e2a386b03da7ed25646a60c6bf6e1706181e793a
Carter Johnson <carreejoh4637@gmail.com>
2025-05-26 16:16:27 -0600

    browser logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -18,9 +18,11 @@ const Header: React.FC = () => {
   return (
     <header className="w-full fixed top-0 z-50 bg-[#272627] shadow-sm border-b-[2px] border-primary">
       <div className="h-24 flex items-center justify-between px-4 sm:px-6 md:px-8 lg:px-24 xl:px-28 2xl:px-48">
-        <Link to="/" className="text-3xl font-bold tracking-wide text-primary italic">
+        <Link to="/" className="">
           <img src={clientConfig.logo}/>
         </Link>
+
+        <h2 className="text-3xl font-bold tracking-wide text-primary italic">AMR</h2>
 
         {/* Desktop nav */}
         <nav className="hidden md:flex space-x-4">
